7ee94a6991d75cce71b3987cbf1a479cc7f7e1c0
Darshana Perera <91814341+darshana-perera-97@users.noreply.github.com>
2025-11-27 11:30:18 +0530

    red theme added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 
-const API_BASE_URL = 'http://69.197.187.24:2028';
+const API_BASE_URL = 'http://93.127.136.228:2028';
+// const API_BASE_URL = 'http://69.197.187.24:2028';
 // const API_BASE_URL = 'http://localhost:2028';
 
 export default API_BASE_URL;
